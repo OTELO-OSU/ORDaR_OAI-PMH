76211bda1ec356777605ab3484c63c2c9ad5fc80
anthonyguiot <guiot1@fst-liec-the-l01.fst.site.univ-lorraine.fr>
2017-08-16 13:53:06 +0200

    Modification handle errorr

--- Backend/src/index.php
+++ Backend/src/index.php
@@ -13,32 +13,40 @@ $app->get('/oai', function ($request, $response,$args) {
 		$allGetVars = $request->getQueryParams();
 		$request= new RequestApi();
 		
-		$legitarg=['verb','identifier','metadataPrefix'];
-		foreach($allGetVars as $key => $param){
-			if (!in_array($key, $legitarg)){
-	    	$xml= $request->BadArgument();
-	    	print $xml;
-	    	return $response->WithHeader("Content-type:","text/xml");					
-			}
-		}
-			
 			
 	    if ($allGetVars['verb']=='Identify') {
+		$legitarg=['verb'];	
 	    	$xml= $request->identify();
-	    	print $xml;
-	    	return $response->WithHeader("Content-type:","text/xml");
+	    	
 	    }
 	    elseif ($allGetVars['verb']=='ListMetadataFormats') {
+	    	$legitarg=['verb','identifier'];	
 	    	$xml= $request->ListMetadataFormats();
-	    	print $xml;
-	    	return $response->WithHeader("Content-type:","text/xml");
 	    }
 	    elseif ($allGetVars['verb']=='ListSets') {
+	    	$legitarg=['verb','resumptionToken'];
 	    	$xml= $request->ListSets();
-	    	print $xml;
-	    	return $response->WithHeader("Content-type:","text/xml");
+	    }
+	    elseif ($allGetVars['verb']=='ListIdentifiers') {
+	    	$legitarg=['verb','metadataPrefix','from','until','set','resumptionToken'];
+	    	if (empty($allGetVars['metadataPrefix'])) {
+	    		$xml= $request->BadArgument();
+	    	}
+	    	else{
+	    		$xml= $request->ListIdentifiers();
+	    	}
+	    }
+	    elseif ($allGetVars['verb']=='ListRecords') {
+	    	$legitarg=['verb','metadataPrefix','from','until','set','resumptionToken'];
+	    	if (empty($allGetVars['metadataPrefix'])) {
+	    		$xml= $request->BadArgument();
+	    	}
+	    	else{
+	    		$xml= $request->ListRecords();
+	    	}
 	    }
 	    elseif ($allGetVars['verb']=='GetRecord') {
+	    	$legitarg=['verb','metadataPrefix','identifier'];
 	    	if (empty($allGetVars['identifier']) OR empty($allGetVars['metadataPrefix'])) {
 	    		$xml= $request->BadArgument();
 		    	
@@ -48,14 +56,29 @@ $app->get('/oai', function ($request, $response,$args) {
 	    		$xml= $request->GetRecord($identifier,$allGetVars['metadataPrefix']);
 	    		
 	    	}
-	    	print $xml;
-	    	return $response->WithHeader("Content-type:","text/xml");
 	    }
 	    else{
 			$xml= $request->IllegalVerb();
 	    	print $xml;
 	    	return $response->WithHeader("Content-type:","text/xml");
+
 	    }
+	    $badarg=0;
+	    	foreach($allGetVars as $key => $param){
+			if (!in_array($key, $legitarg)){
+	    	$xml= $request->BadArgument();
+	    	print $xml;
+	    	return $response->WithHeader("Content-type:","text/xml");					
+			}
+			else{
+				$badarg=1;
+			}
+		}
+		if ($badarg==1) {
+				print $xml;
+			return $response->WithHeader("Content-type:","text/xml");
+			
+		}
 
       
 });
